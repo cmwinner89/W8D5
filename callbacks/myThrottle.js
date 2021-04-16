6324c08755387dad122212f3eec06ff65c0ccf16
Collin Winner <cmwinner89@gmail.com>
2021-04-16 17:33:21 -0400

    work on myThrottle

--- callbacks/myThrottle.js
+++ callbacks/myThrottle.js
@@ -1,11 +1,32 @@
 
 
-Function.prototype.myThrottle(interval) {
-  
+Function.prototype.myThrottle = function(interval) {
+  let tooSoon;
+
+  if (!tooSoon){
+      tooSoon = true;
+      setTimeout(function () {
+          console.log(this)
+          tooSoon = false;
+          this.myThrottle(interval);
+          return this;
+        }, interval); 
+  }
 }
 
+class Neuron {
+    fire() {
+      console.log("Firing!");
+    }
+}
 
+const neuron = new Neuron();
 
+neuron.fire = neuron.fire.myThrottle(500);
+
+const interval = setInterval(() => {
+    neuron.fire();
+}, 10);
 
 
 
