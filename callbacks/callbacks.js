f8b272dbfb64ca9a2a9bdf8e3fcba6ab407dc4a4
fdiengott <freddiengott@gmail.com>
2021-04-16 14:35:54 -0400

    Add functioning Clock class

--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -1,35 +1,42 @@
 class Clock {
     constructor() {
-      // 1. Create a Date object.
-        this.date = new Date(Date());
-      // 2. Store the hours, minutes, and seconds.
-        this.hours = this.date.getHours();
-        this.minutes = this.date.getMinutes();
-        this.seconds = this.date.getSeconds();
-      // 3. Call printTime.
-        this.printTime(); 
-      // 4. Schedule the tick at 1 second intervals.
-      console.log(`this in constructor ${this}`);
-        this._tick();
+      const date = new Date(Date());
+      this.hours = date.getHours();
+      this.minutes = date.getMinutes();
+      this.seconds = date.getSeconds();
+      this.printTime(); 
+      this._tick();
     }
   
     printTime() {
-      // Format the time in HH:MM:SS
-      // Use console.log to print it.
-         this.seconds+=1;
         console.log(`${this.hours} : ${this.minutes} : ${this.seconds}`);
     }
   
     _tick() {
-      // 1. Increment the time by one second.
-      // 2. Call printTime.
       let that = this;
+
       setInterval(function() { 
-        that.hours = that.date.setHours();
-        that.minutes = that.date.setMinutes();
-        that.seconds = that.date.setSeconds();
-        let x = that.printTime.bind(that);
-        x();
+        if (that.seconds === 59) {
+          that.seconds = 0;
+
+          if (that.minutes === 59) {
+            that.minutes = 0; 
+
+            if (that.hours === 23) {
+              that.hours = 0;
+            } else {
+              that.hours += 1;
+            }
+
+          } else {
+            that.minutes += 1; 
+          }
+
+        } else {
+          that.seconds += 1;
+        }
+        
+        that.printTime.call(that);
       }, 1000);
 
 
@@ -37,3 +44,6 @@ class Clock {
   }
   
   const clock = new Clock();
+
+
+  
